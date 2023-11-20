5483f10ab4cb154c7ec44ebb541fa6dc2b42a9c4
sanjeewna <sanjeewnadevi93@gmail.com>
2023-11-20 10:32:07 +0200

    week7-lab2 completed

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// App.js
+function Hello() {
+  return <p>Hello, React!</p>;
+}
+
+// App.js
+function Bye() {
+  return <p>Goodbye, React!</p>;
+}
+
+// App.js
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Bye />
+    </div>
+  );
+}
+
+export default App;
